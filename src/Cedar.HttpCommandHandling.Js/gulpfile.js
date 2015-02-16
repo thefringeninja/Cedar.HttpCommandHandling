2e10be7c20c0e7a6d2bcc278d7b7c2cc6d0561d2
Ronald van der Kooij <ronald@sharpsolutions.nl>
2015-02-16 11:40:51 +0100

    creating distribution versions of the cedarjs files

--- src/Cedar.HttpCommandHandling.Js/gulpfile.js
+++ src/Cedar.HttpCommandHandling.Js/gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
-    karma = require('gulp-karma');
+    karma = require('gulp-karma'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
 gulp.task('jsTests', function () {
     
@@ -12,4 +14,18 @@ gulp.task('jsTests', function () {
         });
 });
 
-gulp.task('default', [ 'jsTests']);
+gulp.task('default', [ 'jsTests', 'jquery-dist', 'angular-dist']);
+
+gulp.task('jquery-dist', function(){
+    gulp.src('./jquery/src/*.js')
+    .pipe(uglify())
+    .pipe(rename('cedar-jquery.min.js'))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('angular-dist', function(){
+    gulp.src('./angular/src/*.js')
+    .pipe(uglify({ mangle: false }))
+    .pipe(rename('cedar-angular.min.js'))
+    .pipe(gulp.dest('dist'))
+});
