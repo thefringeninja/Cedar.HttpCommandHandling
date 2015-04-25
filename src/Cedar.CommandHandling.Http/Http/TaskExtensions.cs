3687e098ba9cd69750cea22222fa7d429c40a351
Damian Hickey <dhickey@gmail.com>
2015-04-25 12:03:18 +0200

    Task timeout extension should not complete on captured context.

--- src/Cedar.CommandHandling.Http/Http/TaskExtensions.cs
+++ src/Cedar.CommandHandling.Http/Http/TaskExtensions.cs
@@ -10,10 +10,15 @@ namespace System.Threading.Tasks
             return task.ConfigureAwait(false);
         }
 
+        internal static ConfiguredTaskAwaitable<TResult> NotOnCapturedContext<TResult>(this Task<TResult> task)
+        {
+            return task.ConfigureAwait(false);
+        }
+
         internal static async Task WithTimeout(this Task task, TimeSpan delay)
         {
             var cts = new CancellationTokenSource();
-            Task completedTask = await Task.WhenAny(task, Task.Delay(delay, cts.Token));
+            Task completedTask = await Task.WhenAny(task, Task.Delay(delay, cts.Token)).NotOnCapturedContext();
             if (completedTask != task)
             {
                 throw new TimeoutException("The operation has timed out.");
