bb5fdca06bbbee85335d89c5def65e13fd90f8da
Damian Hickey <dhickey@gmail.com>
2015-06-09 16:12:29 +0200

    Can get collection of registered command types from a module.

--- src/Cedar.CommandHandling.Tests/CommandHandlerModuleTests.cs
+++ src/Cedar.CommandHandling.Tests/CommandHandlerModuleTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Cedar.CommandHandling
 {
     using System;
+    using System.Linq;
     using System.Threading.Tasks;
     using FluentAssertions;
     using Xunit;
@@ -16,6 +17,19 @@
             Action act = () => module.For<Command>().Handle((_, __) => Task.FromResult(0));
 
             act.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Can_get_command_types()
+        {
+            var module = new CommandHandlerModule();
+            
+            module.For<Command>().Handle((_,__) => Task.FromResult(0));
+
+            var commandTypes = module.CommandTypes.ToList();
+
+            commandTypes.Should().HaveCount(1);
+            commandTypes.Single().Should().Be(typeof(Command));
         }
     }
 }
